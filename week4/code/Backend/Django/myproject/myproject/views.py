1ebe73240106a674779e2d53bca330678bb331d2
Prashanshi <11prashi@gmail.com>
2025-07-08 14:54:34 +0530

    myproject update with templates

--- week4/code/Backend/Django/myproject/myproject/views.py
+++ week4/code/Backend/Django/myproject/myproject/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("✅ Hello, this is the home page!")
+    return render(request,"index.html")
